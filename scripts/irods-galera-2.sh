3a954fd54483b3bb48c919d7064f7175089545dd
Michael J. Stealey <michael.j.stealey@gmail.com>
2017-05-22 18:12:48 -0400

    cleanup dirs

--- scripts/irods-galera-2.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-iadmin mkresc galera2 unixfilesystem irods-galera-2:/Vault
-sed -i 's/"irods_default_resource": "demoResc",/"irods_default_resource": "galera2",/g' /var/lib/irods/.irods/irods_environment.json
-
-exit 0;
